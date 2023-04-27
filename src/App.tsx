df3af14838336514bfd89913c4efd8ef88a49ac0
bautancredi <bautistatancredi@hotmail.com>
2023-04-27 17:03:40 -0300

    title change

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import NewPublicationPage from "./pages/NewPublicationPage";
 function App() {
   return (
     <div className="min-h-screen bg-stone-900 p-8 text-white">
-      <h1 className="m-3 text-center text-3xl font-bold">Publications Display App</h1>
+      <h1 className="m-3 text-center text-3xl font-bold">Publications Display App 2</h1>
       <Routes>
         <Route element={<PublicationDisplayPage />} path="/" />
         <Route element={<NewPublicationPage />} path="/new" />
